e0acffef470f4194c04f26fa4656e70c2b8374cd
Kevin Andrey Herrera Silva <kahs_kevin@hotmail.com>
2020-04-09 23:19:30 -0500

    First Commit

---
+++ ETaxS/ETaxS/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ETaxS
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            //InitializeComponent();
+            Xamarin.Forms.PlatformConfiguration.AndroidSpecific.Application.SetWindowSoftInputModeAdjust(this, Xamarin.Forms.PlatformConfiguration.AndroidSpecific.WindowSoftInputModeAdjust.Resize);
+            WebView webView = new WebView
+            {
+
+                Source = new UrlWebViewSource
+                {
+                    Url = "https://www.exclusivetaxservices.com/Service/User/",
+                },
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+
+            // Accomodate iPhone status bar.
+            this.Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5);
+
+            // Build the page.
+            this.Content = new StackLayout
+            {
+                Children =
+                {
+                    webView
+                }
+            };
+        }
+    }
+}
